6a299227c446dad41151421a2d84fa9f19d36dea
Lucas Sawade <lsawade@princeton.edu>
2018-12-04 12:16:23 -0500

    deleted doxygen stuff

--- docs/html/search/all_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['testgll',['TestGLL',['../classtest__gll_1_1_test_g_l_l.html',1,'test_gll']]],
-  ['testgllpointsandweights2',['testGLLPointsAndWeights2',['../classtest__gll_1_1_test_g_l_l.html#a1357eaab2f6d378ac53e9ae0276d2a2b',1,'test_gll::TestGLL']]],
-  ['testlagrangepoly',['testLagrangePoly',['../classtest__gll_1_1_test_g_l_l.html#a95fd324973c22c9edae81c4adbed0c4f',1,'test_gll::TestGLL']]]
-];
--- docs/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['testgll',['TestGLL',['../classtest__gll_1_1_test_g_l_l.html',1,'test_gll']]]
-];
