be6c373fa4de31575a01e87e1f1e1532eb5c413e
srijaniiserprinceton <43680406+srijaniiserprinceton@users.noreply.github.com>
2018-12-26 12:09:32 -0500

    Add files via upload
    File to show display the local2global conversion according to the example in "fegeo.pdf" equation 5.35 or 5.36

---
+++ tests/test_loc2glob.py
@@ -0,0 +1,15 @@
+from context import src
+import unittest
+import numpy as np
+
+Me = np.ones([4,4])
+Mg = np.zeros([8,8])
+
+node_num = np.array([[4,1,2,5],[5,2,3,6],[7,4,5,8]])-1
+el_no = np.array(range(0,len(node_num)))
+
+for i in el_no:
+    Mg += src.local2global(Me,Mg,node_num,[i])
+
+print(Mg)
+
